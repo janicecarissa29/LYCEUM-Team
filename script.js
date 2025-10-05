6e65a854ca78b55d981a8378528c16f38ddfc8dd
ericshi658-ux <ericshi658@gmail.com>
2025-10-05 20:46:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ navbarToggle.addEventListener('click', ()=>{
   navbarToggle.classList.toggle('active');
   navbarMenu.classList.toggle('active');
 })
+
+function handleLogout() {
+    localStorage.removeItem('lyceum_isLoggedIn'); 
+}
+
+function handleLogout() {
+    localStorage.removeItem('lyceum_isLoggedIn'); 
+    checkLoginStatus(); 
+    window.location.href = 'index.html'; 
+
